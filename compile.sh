404cea8dc8005f591b327fccc420bf3deee45beb
thomas.mccolgan <thomas.mccolgan@269e7166-274e-0410-8643-07c3541e3e83>
2008-08-19 16:49:51 +0000

    made some adjustments to the random generation and error handling

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 if [ "$(uname -s)" == 'Darwin' ];
 then
 g95 -O1 -o main nrutil.f90 gaussj.f90 jacobi.f90 nrtype.f90 indexx.f90 nr.f90  main.f90;
+else
+gfortran -O1 -o main nrutil.f90 gaussj.f90 jacobi.f90 nrtype.f90 indexx.f90 nr.f90  main.f90;
 fi;
